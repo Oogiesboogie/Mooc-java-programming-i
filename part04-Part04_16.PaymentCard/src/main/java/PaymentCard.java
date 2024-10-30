4d9ca8ffe3a90926bb6fadcc37b185bb9f074fe4
Oogiesboogie <splath1@students.kennesaw.edu>
2024-10-30 18:52:29 -0400

    Add files via upload

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sjp12
+ */
+public class PaymentCard {
+    private double balance;
+    
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+    
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+           balance -= 2.60; 
+        }
+    }
+    
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            balance -= 4.60;
+        }
+    }
+    
+    public void addMoney(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            if (balance > 150) {
+                balance = 150;
+            }
+        }
+    }
+    
+    public String toString() {
+        return "The card has a balance of " + balance + " euros";
+    }
+    
+}
